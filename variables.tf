23d1f92d8c323b5eba6927c3ed44e362b425c4fe
mahesh <maheshthota2136@gmail.com>
2023-09-23 18:01:34 +0530

    roboshop-aws-security-group

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "sg_name" {
+}
+
+variable "sg_description" {
+    default = ""
+}
+
+variable "common_tags"{
+}
+
+variable "sg_tags" {
+}
+
+variable "project_name" {
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "sg_ingress_rules" {
+  type = list
+  default = []
+}
